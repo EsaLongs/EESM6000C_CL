00dbf9ba9152f5f5c63ad0d76ffbbc8003866c3e
EsaLongs <478113618@qq.com>
2025-02-17 15:21:53 +0800

    a

--- w-2_homework/code_rewrite.sv
+++ w-2_homework/code_rewrite.sv
@@ -10,3 +10,9 @@ assign a = ((addr & mask) == My_addr) ? b[1] : !b[0];
 
 // There must not be a space between dimensions
 logic [7:0][3:0] data[128][2];
+
+// There must be a space around packed dimensions
+typedef logic [31:0] word_t;
+
+// Ternary expressions nested in the true condition of another ternary expression must be enclosed in parentheses
+assign foo = condition_a ? (condition_a_x ? x : y) : b;
