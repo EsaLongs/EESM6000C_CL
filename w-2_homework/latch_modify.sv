a271d378da18b48178c3fac499bcfc12ccbf7c6e
EsaLongs <478113618@qq.com>
2025-02-17 17:09:56 +0800

    a

--- w-2_homework/latch_modify.sv
+++ w-2_homework/latch_modify.sv
@@ -1,4 +1,5 @@
 //---------------------- Original Code -----------------------//
+  //---------------------- Code 1 ----------------------------//
   always @(*) begin : EX22_PROC
     case (c)
       1'b0 : begin
@@ -10,11 +11,30 @@
       end
     endcase
   end
-
   // This code will generate latch,for z doesn't be assigned 
   // any value in default part, which causes z always be 1'b0.
 
+  //---------------------- Code 2 ----------------------------//
+  always @(*) begin
+    case (d)
+      2'b00 : z <= 1'b1;
+      2'b01 : z <= 1'b0;
+      2'b10 : begin
+              z <= 1'b1;
+              s <= 1'b1;
+      end 
+      default : begin
+              z <= 1'b0;
+              s <= 1'b0;
+      end
+    endcase
+  end
+
+  // This code will generate latch,for s doesn't be assigned 
+  // any value when (d == 2'b00) or (d == 2'b01).
+
 //---------------------- Modified Code -----------------------//
+  //---------------------- Code 1 ----------------------------//
   always @(*) begin : EX22_PROC
     case (c)
       1'b0 : begin
@@ -24,6 +44,28 @@
       default: begin
         q = 1'b0;
         z = 1'b1;
+      end
+    endcase
+  end
+
+  //---------------------- Code 2 ----------------------------//
+  always @(*) begin
+    case (d)
+      2'b00 : begin
+              z <= 1'b1;
+              s <= 1'b0;
+      end
+      2'b01 : begin
+              z <= 1'b0;
+              s <= 1'b0;
+      end
+      2'b10 : begin
+              z <= 1'b1;
+              s <= 1'b1;
+      end 
+      default : begin
+              z <= 1'b0;
+              s <= 1'b0;
       end
     endcase
   end
