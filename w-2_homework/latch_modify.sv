8b576b6aa41474df72998028604c4e0e64189a7f
EsaLongs <478113618@qq.com>
2025-02-17 17:02:26 +0800

    a

--- w-2_homework/latch_modify.sv
+++ w-2_homework/latch_modify.sv
@@ -1,1 +1,29 @@
 //---------------------- Original Code -----------------------//
+  always @(*) begin : EX22_PROC
+    case (c)
+      1'b0 : begin
+        q = 1'b1;
+        z = 1'b0;
+      end
+      default: begin
+        q = 1'b0;
+      end
+    endcase
+  end
+
+  // This code will generate latch,for z doesn't be assigned 
+  // any value in default part, which causes z always be 1'b0.
+
+//---------------------- Modified Code -----------------------//
+  always @(*) begin : EX22_PROC
+    case (c)
+      1'b0 : begin
+        q = 1'b1;
+        z = 1'b0;
+      end
+      default: begin
+        q = 1'b0;
+        z = 1'b1;
+      end
+    endcase
+  end
