982568f5da17b37f34cbdd3185c96d36d1c2cd7a
EsaLongs <478113618@qq.com>
2025-02-18 23:22:08 +0800

    a

--- Lab_3/Brent_Kung_Adder.sv
+++ Lab_3/Brent_Kung_Adder.sv
@@ -1,9 +1,9 @@
 `include "define.sv"
-module gp_unit (
+module _unit (
   input  logic in_g1, in_g2,
   input  logic in_p1, in_p2,
   output logic out_g,
-  output logic out_p,
+  output logic out_p
 );
 
   assign out_g = in_g2 || (in_p2 && in_g1);
@@ -20,6 +20,8 @@ module brent_kung_adder (
   output logic cout
 );
 
+  genvar i;
+
 //------------------------ Stage 0 -------------------------//
   logic [`ADDER_SIZE - 1 : 0] P0;  // Propagate signal
   logic [`ADDER_SIZE - 1 : 0] G0;  // Generate signal
@@ -27,120 +29,126 @@ module brent_kung_adder (
   assign G0 = in_op1 & in_op2;
 
 //------------------------ Stage 1 -------------------------//
-  logic [`ADDER_SIZE / 2 - 1 : 0] P1;
-  logic [`ADDER_SIZE / 2 - 1 : 0] G1;
-  
-  genvar i;
   generate
-    for (i = 0; i < (`ADDER_SIZE / 2); i = i + 1) begin : STAGE_1
-      gp_unit stage_1_gp (
-        .in_g1  ( G0[i * 2]     ),
-        .in_g2  ( G0[i * 2 + 1] ),
-        .in_p1  ( P0[i * 2]     ),
-        .in_p2  ( P0[i * 2 + 1] ),
-        .out_g  ( G1[i]         ),
-        .out_p  ( P1[i]         )
-      );
+    if (`ADDER_SIZE > (1 - 1)) begin
+
+      logic [`ADDER_SIZE / 2 - 1 : 0] P1;
+      logic [`ADDER_SIZE / 2 - 1 : 0] G1;
+
+      for (i = 0; i < (`ADDER_SIZE / 2); i = i + 1) begin : STAGE_1
+        gp_unit stage_1_gp (
+          .in_g1  ( G0[i * 2]     ),
+          .in_g2  ( G0[i * 2 + 1] ),
+          .in_p1  ( P0[i * 2]     ),
+          .in_p2  ( P0[i * 2 + 1] ),
+          .out_g  ( G1[i]         ),
+          .out_p  ( P1[i]         )
+        );
+      end
     end
   endgenerate
 
-//------------------------ Stage 2 -------------------------//
-  logic [`ADDER_SIZE / 4 - 1 : 0] P2;
-  logic [`ADDER_SIZE / 4 - 1 : 0] G2;
-  
+//------------------------ Stage 2 -------------------------//  
   generate
-    for (i = 0; i < (`ADDER_SIZE / 4); i = i + 1) begin : STAGE_2
-      gp_unit stage_2_gp (
-        .in_g1  ( G1[i * 2]     ),
-        .in_g2  ( G1[i * 2 + 1] ),
-        .in_p1  ( P1[i * 2]     ),
-        .in_p2  ( P1[i * 2 + 1] ),
-        .out_g  ( G2[i]         ),
-        .out_p  ( P2[i]         )
-      );
+    if (`ADDER_SIZE > (4 - 1)) begin
+
+      logic [`ADDER_SIZE / 4 - 1 : 0] P2;
+      logic [`ADDER_SIZE / 4 - 1 : 0] G2;
+
+      for (i = 0; i < (`ADDER_SIZE / 4); i = i + 1) begin : STAGE_2
+        gp_unit stage_2_gp (
+          .in_g1  ( G1[i * 2]     ),
+          .in_g2  ( G1[i * 2 + 1] ),
+          .in_p1  ( P1[i * 2]     ),
+          .in_p2  ( P1[i * 2 + 1] ),
+          .out_g  ( G2[i]         ),
+          .out_p  ( P2[i]         )
+        );
+      end
     end
   endgenerate
 
 //------------------------ Stage 3 -------------------------//
-  logic [`ADDER_SIZE / 8 - 1 : 0] P3;
-  logic [`ADDER_SIZE / 8 - 1 : 0] G3;
-  
   generate
-    for (i = 0; i < (`ADDER_SIZE / 8); i = i + 1) begin : STAGE_3
-      gp_unit stage_3_gp (
-        .in_g1  ( G2[i * 2]     ),
-        .in_g2  ( G2[i * 2 + 1] ),
-        .in_p1  ( P2[i * 2]     ),
-        .in_p2  ( P2[i * 2 + 1] ),
-        .out_g  ( G3[i]         ),
-        .out_p  ( P3[i]         )
-      );
-  end
+    if (`ADDER_SIZE > (8 - 1)) begin
+      logic [`ADDER_SIZE / 8 - 1 : 0] P3;
+      logic [`ADDER_SIZE / 8 - 1 : 0] G3;
+
+      for (i = 0; i < (`ADDER_SIZE / 8); i = i + 1) begin : STAGE_3
+        gp_unit stage_3_gp (
+          .in_g1  ( G2[i * 2]     ),
+          .in_g2  ( G2[i * 2 + 1] ),
+          .in_p1  ( P2[i * 2]     ),
+          .in_p2  ( P2[i * 2 + 1] ),
+          .out_g  ( G3[i]         ),
+          .out_p  ( P3[i]         )
+        );
+      end
+    end
   endgenerate
 
 //------------------------ Stage 4 -------------------------//
-  logic [`ADDER_SIZE / 16 - 1 : 0] P4;
-  logic [`ADDER_SIZE / 16 - 1 : 0] G4;
-  
   generate
-    for (i = 0; i < (`ADDER_SIZE / 16); i = i + 1) begin : STAGE_4
-      gp_unit stage_4_gp (
-        .in_g1  ( G3[i*2]   ),
-        .in_g2  ( G3[i*2+1] ),
-        .in_p1  ( P3[i*2]   ),
-        .in_p2  ( P3[i*2+1] ),
-        .out_g  ( G4[i]     ),
-        .out_p  ( P4[i]     )
-      );
+    if (`ADDER_SIZE > (16 - 1)) begin
+      
+      logic [`ADDER_SIZE / 16 - 1 : 0] P4;
+      logic [`ADDER_SIZE / 16 - 1 : 0] G4;
+
+      for (i = 0; i < (`ADDER_SIZE / 16); i = i + 1) begin : STAGE_4
+        gp_unit stage_4_gp (
+          .in_g1  ( G3[i*2]   ),
+          .in_g2  ( G3[i*2+1] ),
+          .in_p1  ( P3[i*2]   ),
+          .in_p2  ( P3[i*2+1] ),
+          .out_g  ( G4[i]     ),
+          .out_p  ( P4[i]     )
+        );
+      end
     end
   endgenerate
 
 //------------------------ Stage 5 -------------------------//
-  logic [`ADDER_SIZE / 32 - 1 : 0] P5;
-  logic [`ADDER_SIZE / 32 - 1 : 0] G5;
-  
   generate
-    for (i = 0; i < (`ADDER_SIZE / 32); i = i + 1) begin : STAGE_5
-      gp_unit stage_5_gp (
-        .in_g1  ( G4[i * 2]     ),
-        .in_g2  ( G4[i * 2 + 1] ),
-        .in_p1  ( P4[i * 2]     ),
-        .in_p2  ( P4[i * 2 + 1] ),
-        .out_g  ( G5[i]         ),
-        .out_p  ( P5[i]         )
-      );
+    if (`ADDER_SIZE > (32 - 1)) begin
+
+      logic [`ADDER_SIZE / 32 - 1 : 0] P5;
+      logic [`ADDER_SIZE / 32 - 1 : 0] G5;
+
+      for (i = 0; i < (`ADDER_SIZE / 32); i = i + 1) begin : STAGE_5
+        gp_unit stage_5_gp (
+          .in_g1  ( G4[i * 2]     ),
+          .in_g2  ( G4[i * 2 + 1] ),
+          .in_p1  ( P4[i * 2]     ),
+          .in_p2  ( P4[i * 2 + 1] ),
+          .out_g  ( G5[i]         ),
+          .out_p  ( P5[i]         )
+        );
+      end
     end
   endgenerate
 
 //------------------------ Stage 6 -------------------------//
-  logic [`ADDER_SIZE / 64 - 1 : 0] P6;
-  logic [`ADDER_SIZE / 64 - 1 : 0] G6;
-  
   generate
-    for (i = 0; i < (`ADDER_SIZE / 64); i = i + 1) begin: STAGE_6
-      gp_unit stage_6_gp (
-        .in_g1  ( G5[i * 2]     ),
-        .in_g2  ( G5[i * 2 + 1] ),
-        .in_p1  ( P5[i * 2]     ),
-        .in_p2  ( P5[i * 2 + 1] ),
-        .out_g  ( G6[i]         ),
-        .out_p  ( P6[i]         )
-      );
+    if (`ADDER_SIZE > (64 - 1)) begin
+
+      logic [`ADDER_SIZE / 64 - 1 : 0] P6;
+      logic [`ADDER_SIZE / 64 - 1 : 0] G6;
+
+      for (i = 0; i < (`ADDER_SIZE / 64); i = i + 1) begin : STAGE_5
+        gp_unit stage_5_gp (
+          .in_g1  ( G4[i * 2]     ),
+          .in_g2  ( G4[i * 2 + 1] ),
+          .in_p1  ( P4[i * 2]     ),
+          .in_p2  ( P4[i * 2 + 1] ),
+          .out_g  ( G5[i]         ),
+          .out_p  ( P5[i]         )
+        );
+      end
     end
   endgenerate
 
 //------------------------ Calculate Carry in --------------//
   logic [`ADDER_SIZE - 1 : 0] CIN;
-  
-  //------------------------ Highest bit Carry in ----------//
-  assign CIN[`ADDER_SIZE - 1] = (G0[`ADDER_SIZE - 1])
-                             || (G0[`ADDER_SIZE - 2]      && P0[`ADDER_SIZE - 1]) 
-                             || (G1[`ADDER_SIZE / 2 - 1]  && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2]) 
-                             || (G2[`ADDER_SIZE / 4 - 1]  && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1]) 
-                             || (G3[`ADDER_SIZE / 8 - 1]  && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1]) 
-                             || (G4[`ADDER_SIZE / 16 - 1] && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1] && P3[`ADDER_SIZE / 8 - 1]) 
-                             || (G5[`ADDER_SIZE / 32 - 1] && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1] && P3[`ADDER_SIZE / 8 - 1] && P4[`ADDER_SIZE / 16 - 1]) 
-                             || (G6[`ADDER_SIZE / 64 - 1] && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1] && P3[`ADDER_SIZE / 8 - 1] && P4[`ADDER_SIZE / 16 - 1] && P5[`ADDER_SIZE / 32 - 1]);
   
   //------------------------ Lowest bit Carry in -----------//
   assign CIN[0] = cin;
@@ -151,12 +159,7 @@ module brent_kung_adder (
   ////////////////////////////////////////////////////////////
   generate
     for (i = 32; i < (`ADDER_SIZE - 1); i = i + 32) begin
-      assign CIN[i] = (G0[i]) 
-                   || (G0[i + 1]        && P0[i])
-                   || (G1[(i + 2) / 4]  && P0[i] && P0[i + 1]) 
-                   || (G2[(i + 4) / 8]  && P0[i] && P0[i + 1] && P1[(i + 2) / 2]) 
-                   || (G3[(i + 8) / 16] && P0[i] && P0[i + 1] && P1[(i + 2) / 2] && P2[(i + 4) / 4]) 
-                   || (G4[i / 32]       && P0[i] && P0[i + 1] && P1[(i + 2) / 2] && P2[(i + 4) / 4] && P3[(i + 8) / 16]);
+      assign CIN[i] = G5[0] || (P5[0] && cin);
     end
   endgenerate
 
@@ -165,11 +168,7 @@ module brent_kung_adder (
   ////////////////////////////////////////////////////////////
   generate
     for (i = 16; i < (`ADDER_SIZE - 1); i = i + 32) begin
-        assign CIN[i] = (G0[i])
-                     || (G0[i + 1]       && P0[i]) 
-                     || (G1[(i + 2) / 4] && P0[i] && P0[i + 1]) 
-                     || (G2[(i + 4) / 8] && P0[i] && P0[i + 1] && P1[(i + 2) / 2]) 
-                     || (G3[i / 16]      && P0[i] && P0[i + 1] && P1[(i + 2) / 2] && P2[(i + 4) / 4]);
+        assign CIN[i] = G4[0] || (P5[0] && cin);
     end
   endgenerate  
       
@@ -210,12 +209,12 @@ module brent_kung_adder (
   ////////////////////////////////////////////////////////////
   generate
     for (i = 1; i < (`ADDER_SIZE - 1); i = i + 2) begin  
-      assign CIN[i] = G0[i] || (G0[i - 1] && P0[i]);
+      assign CIN[i] = G0[i - 1] || (P0[i - 1] && CIN[i - 1]);
     end
   endgenerate 
 
 //------------------------ Calculate Result ----------------//
     assign out_res = P0 ^ CIN;
-    assign cout = G0[`ADDER_SIZE - 1] || (P0[`ADDER_SIZE - 1] && CIN[`ADDER_SIZE - 1]);
+    assign cout = G6 || (P6 && cin);
 
 endmodule
