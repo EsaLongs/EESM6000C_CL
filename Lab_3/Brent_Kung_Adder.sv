0b7d0f2684e2484c23443ce5433eae05668ee3c3
EsaLongs <478113618@qq.com>
2025-02-18 17:42:06 +0800

    a

--- Lab_3/Brent_Kung_Adder.sv
+++ Lab_3/Brent_Kung_Adder.sv
@@ -1,99 +1,211 @@
 `include "define.sv"
 module adder_unit (
-  input  logic g1, g2,
-  input  logic p1, p2,
-  output logic gout,
-  output logic pout,
+  input  logic in_g1, in_g2,
+  input  logic in_p1, in_p2,
+  output logic out_g,
+  output logic out_p,
 );
 
-  assign gout = g2 || (p2 && g1);
-  assign pout = p2 && p1;
+  assign out_g = in_g2 || (in_p2 && in_g1);
+  assign out_p = in_p2 && in_p1;
+
 endmodule
 
 
 module brent_kung_adder (
-  input  logic [`ADDER_SIZE-1:0] in_op1,
-  input  logic [`ADDER_SIZE-1:0] in_op2,
-  output logic [`ADDER_SIZE-1:0] out_res,
+  input  logic [`ADDER_SIZE - 1 : 0] in_op1,
+  input  logic [`ADDER_SIZE - 1 : 0] in_op2,
+  output logic [`ADDER_SIZE - 1 : 0] out_res,
 
-  input  logic Cin,
-  output logic Cout
+  input  logic cin,
+  output logic cout
 );
 
 //------------------------ Stage 0 -------------------------//
-  logic [`ADDER_SIZE-1:0] P0;  // Propagate signal
-  logic [`ADDER_SIZE-1:0] G0;  // Generate signal
+  logic [`ADDER_SIZE - 1 : 0] P0;  // Propagate signal
+  logic [`ADDER_SIZE - 1 : 0] G0;  // Generate signal
   assign P0 = in_op1 ^ in_op2;
   assign G0 = in_op1 & in_op2;
+
 //------------------------ Stage 1 -------------------------//
+  logic [`ADDER_SIZE / 2 - 1 : 0] P1;
+  logic [`ADDER_SIZE / 2 - 1 : 0] G1;
+  
+  genvar i;
+  generate
+    for (i = 0; i < (`ADDER_SIZE / 2); i = i + 1) begin : STAGE_1
+      adder_unit stage_1_gp(
+        .in_g1  ( G0[i * 2]     ),
+        .in_g2  ( G0[i * 2 + 1] ),
+        .in_p1  ( P0[i * 2]     ),
+        .in_p2  ( P0[i * 2 + 1] ),
+        .out_g  ( G1[i]         ),
+        .out_p  ( P1[i]         )
+      );
+    end
+  endgenerate
 
-    // 第一步：计算传播和生成信号
- 
+//------------------------ Stage 2 -------------------------//
+  logic [`ADDER_SIZE / 4 - 1 : 0] P2;
+  logic [`ADDER_SIZE / 4 - 1 : 0] G2;
+  
+  generate
+    for (i = 0; i < (`ADDER_SIZE / 4); i = i + 1) begin : STAGE_2
+      adder_unit stage_2_gp(
+        .in_g1  ( G1[i * 2]     ),
+        .in_g2  ( G1[i * 2 + 1] ),
+        .in_p1  ( P1[i * 2]     ),
+        .in_p2  ( P1[i * 2 + 1] ),
+        .out_g  ( G2[i]         ),
+        .out_p  ( P2[i]         )
+      );
+    end
+  endgenerate
 
-    // 第二步：计算进位信号（in_op2rent - Kung算法）
-    // 级别1
-    logic [15:0] P1;
-    logic [15:0] G1;
-    generate
-        genvar i;
-        for (i = 0; i < 16; i = i + 1) begin
-            assign P1[i] = P[2*i] & P[2*i+1];
-            assign G1[i] = G[2*i+1] | (G[2*i] & P[2*i+1]);
-        end
-    endgenerate
+//------------------------ Stage 3 -------------------------//
+  logic [`ADDER_SIZE / 8 - 1 : 0] P3;
+  logic [`ADDER_SIZE / 8 - 1 : 0] G3;
+  
+  generate
+    for (i = 0; i < (`ADDER_SIZE / 8); i = i + 1) begin : STAGE_3
+      adder_unit stage_3_gp(
+        .in_g1  ( G2[i * 2]     ),
+        .in_g2  ( G2[i * 2 + 1] ),
+        .in_p1  ( P2[i * 2]     ),
+        .in_p2  ( P2[i * 2 + 1] ),
+        .out_g  ( G3[i]         ),
+        .out_p  ( P3[i]         )
+      );
+  end
+  endgenerate
 
-    // 级别2
-    logic [7:0] P2;
-    logic [7:0] G2;
-    generate
-        for (i = 0; i < 8; i = i + 1) begin
-            assign P2[i] = P1[2*i] & P1[2*i+1];
-            assign G2[i] = G1[2*i+1] | (G1[2*i] & P1[2*i+1]);
-        end
-    endgenerate
+//------------------------ Stage 4 -------------------------//
+  logic [`ADDER_SIZE / 16 - 1 : 0] P4;
+  logic [`ADDER_SIZE / 16 - 1 : 0] G4;
+  
+  generate
+    for (i = 0; i < (`ADDER_SIZE / 16); i = i + 1) begin : STAGE_4
+      adder_unit stage_4_gp(
+        .in_g1  ( G3[i*2]   ),
+        .in_g2  ( G3[i*2+1] ),
+        .in_p1  ( P3[i*2]   ),
+        .in_p2  ( P3[i*2+1] ),
+        .out_g  ( G4[i]     ),
+        .out_p  ( P4[i]     )
+      );
+    end
+  endgenerate
 
-    // 级别3
-    logic [3:0] P3;
-    logic [3:0] G3;
-    generate
-        for (i = 0; i < 4; i = i + 1) begin
-            assign P3[i] = P2[2*i] & P2[2*i+1];
-            assign G3[i] = G2[2*i+1] | (G2[2*i] & P2[2*i+1]);
-        end
-    endgenerate
+//------------------------ Stage 5 -------------------------//
+  logic [`ADDER_SIZE / 32 - 1 : 0] P5;
+  logic [`ADDER_SIZE / 32 - 1 : 0] G5;
+  
+  generate
+    for (i = 0; i < (`ADDER_SIZE / 32); i = i + 1) begin : STAGE_5
+      adder_unit stage_5_gp(
+        .in_g1  ( G4[i * 2]     ),
+        .in_g2  ( G4[i * 2 + 1] ),
+        .in_p1  ( P4[i * 2]     ),
+        .in_p2  ( P4[i * 2 + 1] ),
+        .out_g  ( G5[i]         ),
+        .out_p  ( P5[i]         )
+      );
+    end
+  endgenerate
 
-    // 级别4
-    logic [1:0] P4;
-    logic [1:0] G4;
-    assign P4[0] = P3[0] & P3[1];
-    assign G4[0] = G3[1] | (G3[0] & P3[1]);
-    assign P4[1] = P3[2] & P3[3];
-    assign G4[1] = G3[3] | (G3[2] & P3[3]);
+//------------------------ Stage 6 -------------------------//
+  logic [`ADDER_SIZE / 64 - 1 : 0] P6;
+  logic [`ADDER_SIZE / 64 - 1 : 0] G6;
+  
+  generate
+    for (i = 0; i < (`ADDER_SIZE / 64); i = i + 1) begin: STAGE_6
+      adder_unit stage_6_gp(
+        .in_g1  ( G5[i * 2]     ),
+        .in_g2  ( G5[i * 2 + 1] ),
+        .in_p1  ( P5[i * 2]     ),
+        .in_p2  ( P5[i * 2 + 1] ),
+        .out_g  ( G6[i]         ),
+        .out_p  ( P6[i]         )
+      );
+    end
+  endgenerate
 
-    // 级别5
-    logic P5;
-    logic G5;
-    assign P5 = P4[0] & P4[1];
-    assign G5 = G4[1] | (G4[0] & P4[1]);
+//------------------------ Calculate Carry in --------------//
+  logic [`ADDER_SIZE - 1 : 0] CIN;
+  
+  //------------------------ Highest bit Carry in ----------//
+  assign CIN[`ADDER_SIZE - 1] = (G0[`ADDER_SIZE - 1])
+                             || (G0[`ADDER_SIZE - 2]      && P0[`ADDER_SIZE - 1]) 
+                             || (G1[`ADDER_SIZE / 2 - 1]  && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2]) 
+                             || (G2[`ADDER_SIZE / 4 - 1]  && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1]) 
+                             || (G3[`ADDER_SIZE / 8 - 1]  && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1]) 
+                             || (G4[`ADDER_SIZE / 16 - 1] && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1] && P3[`ADDER_SIZE / 8 - 1]) 
+                             || (G5[`ADDER_SIZE / 32 - 1] && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1] && P3[`ADDER_SIZE / 8 - 1] && P4[`ADDER_SIZE / 16 - 1]) 
+                             || (G6[`ADDER_SIZE / 64 - 1] && P0[`ADDER_SIZE - 1] && P0[`ADDER_SIZE - 2] && P1[`ADDER_SIZE / 2 - 1] && P2[`ADDER_SIZE / 4 - 1] && P3[`ADDER_SIZE / 8 - 1] && P4[`ADDER_SIZE / 16 - 1] && P5[`ADDER_SIZE / 32 - 1]);
+  
+  //------------------------ Lowest bit Carry in -----------//
+  assign CIN[0] = cin;
 
-    // 回溯计算进位信号
-    // 最高位进位
-    assign C[`ADDER_SIZE-1] = G[`ADDER_SIZE-1] | (G[30] & P[`ADDER_SIZE-1]) | (G2[7] & P[`ADDER_SIZE-1] & P[30]) | (G3[3] & P[`ADDER_SIZE-1] & P[30] & P1[15]) | (G4[1] & P[`ADDER_SIZE-1] & P[30] & P1[15] & P2[7]) | (G5 & P[`ADDER_SIZE-1] & P[30] & P1[15] & P2[7] & P3[3]);
-    // 其他进位
-    generate
-        for (i = 30; i > 0; i = i - 1) begin
-            if (i % 2 == 1) begin
-                assign C[i] = G[i] | (G[i-1] & P[i]);
-            end else if (i % 4 == 0) begin
-                assign C[i] = G[i] | (G[i+1] & P[i]) | (G2[i/4] & P[i] & P[i+1]);
-            end else if (i % 8 == 0) begin
-                assign C[i] = G[i] | (G[i+1] & P[i]) | (G2[(i+2)/4] & P[i] & P[i+1]) | (G3[i/8] & P[i] & P[i+1] & P1[(i+2)/2]);
-            end else if (i % 16 == 0) begin
-                assign C[i] = G[i] | (G[i+1] & P[i]) | (G2[(i+2)/4] & P[i] & P[i+1]) | (G3[(i+4)/8] & P[i] & P[i+1] & P1[(i+2)/2]) | (G4[i/16] & P[i] & P[i+1] & P1[(i+2)/2] & P2[(i+4)/4]);
-            end
-        end
-    endgenerate
-    assign C[0] = Cin;
+  //------------------------ Other bits Carry in -----------//
+  ////////////////////////////////////////////////////////////
+  // Odd bits
+  ////////////////////////////////////////////////////////////
+  generate
+    for (i = 1; i < (`ADDER_SIZE - 1); i = i + 2) begin
+      assign CIN[i] = G0[i] || (G0[i - 1] && P0[i]);
+    end
+  endgenerate
+
+  ////////////////////////////////////////////////////////////
+  // Bits that divisible by 4
+  ////////////////////////////////////////////////////////////
+  generate
+    for (i = 4; i < (`ADDER_SIZE - 1); i = i + 4) begin
+      assign CIN[i] = (G0[i])
+                   || (G0[i + 1] && P0[i]) 
+                   || (G1[i / 4] && P0[i] && P0[i + 1]);
+    end
+  endgenerate
+
+  ////////////////////////////////////////////////////////////
+  // Bits that divisible by 8
+  ////////////////////////////////////////////////////////////
+  generate
+    for (i = 8; i < (`ADDER_SIZE - 1); i = i + 8) begin
+      assign CIN[i] = (G0[i])
+                   || (G0[i + 1] && P0[i]) 
+                   || (G1[(i + 2) / 4] && P0[i] && P0[i + 1]) 
+                   || (G2[i / 8] && P0[i] && P0[i + 1] && P1[(i + 2) / 2]);
+    end
+  endgenerate
+
+  ////////////////////////////////////////////////////////////
+  // Bits that divisible by 16
+  ////////////////////////////////////////////////////////////
+  generate
+    for (i = 16; i < (`ADDER_SIZE - 1); i = i + 16) begin
+      assign CIN[i] = (G0[i])
+                   || (G0[i + 1]       && P0[i]) 
+                   || (G1[(i + 2) / 4] && P0[i] && P0[i + 1]) 
+                   || (G2[(i + 4) / 8] && P0[i] && P0[i + 1] && P1[(i + 2) / 2]) 
+                   || (G3[i / 16]      && P0[i] && P0[i + 1] && P1[(i + 2) / 2] && P2[(i + 4) / 4]);
+    end
+  endgenerate
+
+  ////////////////////////////////////////////////////////////
+  // Bits that divisible by 32
+  ////////////////////////////////////////////////////////////
+  generate
+    for (i = 32; i < (`ADDER_SIZE - 1); i = i + 32) begin
+      assign CIN[i] = (G0[i]) 
+                   || (G0[i + 1]        && P0[i])
+                   || (G1[(i + 2) / 4]  && P0[i] && P0[i + 1]) 
+                   || (G2[(i + 4) / 8]  && P0[i] && P0[i + 1] && P1[(i + 2) / 2]) 
+                   || (G3[(i + 8) / 16] && P0[i] && P0[i + 1] && P1[(i + 2) / 2] && P2[(i + 4) / 4]) 
+                   || (G4[i / 32]       && P0[i] && P0[i + 1] && P1[(i + 2) / 2] && P2[(i + 4) / 4] && P3[(i + 8) / 16]);
+    end
+  endgenerate
+
 
     // 第三步：计算和
     assign out_res = P ^ C;
