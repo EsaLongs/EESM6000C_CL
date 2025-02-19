25e6e9f8cf108bbc328d6a104bc9fc7130d1f7e9
EsaLongs <478113618@qq.com>
2025-02-19 22:34:30 +0800

    a

--- Lab_3/Brent_Kung_Adder_nbit.sv
+++ Lab_3/Brent_Kung_Adder_nbit.sv
@@ -1,11 +1,1 @@
-module brent_kung_adder_nbit (
-  input  logic in_g1, in_g2,
-  input  logic in_p1, in_p2,
-  output logic out_g,
-  output logic out_p
-);
-
-  assign out_g = in_g2 || (in_p2 && in_g1);
-  assign out_p = in_p2 && in_p1;
-
-endmodule
+`define ADDER_SIZE 64
