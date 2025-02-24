63b38bca61495560719828fd1b21051f3562a3ea
EsaLongs <478113618@qq.com>
2025-02-24 23:45:13 +0800

    a

--- axi4_lite copy.sv
+++ axi4_lite copy.sv
@@ -3,23 +3,23 @@
 // Engineer: TANG Yue
 // 
 // Create Date: 23.02.2025 18:20:29
-// Design Name: AXI4 Lite
-// Module Name: axi4_lite_slave
+// Design Name: AXI4 Lite for BRAM
+// Module Name: axi4_lite_slave_bram
 // Project Name: 
 // Target Devices: 
 // Tool Versions: Vivado 2023.1
-// Description: This is a AXI4 Lite interface for salve
+// Description: This is a AXI4 Lite interface for slave
 // 
 // Dependencies:
 // 
 // Revision: 0.01
 // 
-// Additional Comments: Nothing
+// Additional Comments: This kind of axi4 lite is used for a simple BRAM.
 // 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-module axi4_lite_slave #(
-  parameter ADDR_WIDTH = 32,
+module axi4_lite_slave_bram #(
+  parameter ADDR_WIDTH = 5,
   parameter DATA_WIDTH = 32
   ) (
 //------------------------ Global Signals -------------------------------------------//
@@ -42,7 +42,7 @@ module axi4_lite_slave #(
   output logic [1 : 0] out_s_rresp,   // Read response, indicating status of data transfer
   
   output logic out_s_rvalid,   // Indicate the output read data is valid
-  input  logic in_s_rready,    // Indicate master is ready to recieve the data
+  input  logic in_s_rready,    // Indicate master is ready to receive the data
 
 //------------------------ Write Address Channel ------------------------------------//
   input  logic [ADDR_WIDTH - 1 : 0] in_s_awaddr,   // Write address
@@ -57,14 +57,24 @@ module axi4_lite_slave #(
 //------------------------ Write Data Channel ---------------------------------------//
   input  logic [DATA_WIDTH - 1 : 0] in_s_wdata,   // Write data
   
-  // Write strobes, indicating which byte(s) is(are) valid
-  input  logic [DATA_WIDTH / 8 - 1 : 0] in_s_wstrb,
+  input  logic in_s_wvalid,    // Indicate the write data from master is valid
+  output logic out_s_wready,   // Indicate slave is ready to get data
 
 //------------------------ Write Response Channel -----------------------------------//
-  output logic [1 : 0] out_s_bresp,   // Indicating status of write data transfer
+  output logic [1 : 0] out_s_bresp,   // Indicate status of write data transfer
 
   output logic out_s_bvalid,   // Indicate the response from slave is valid
-  input  logic in_s_bready     // Indicate the master is ready to recieve response
+  input  logic in_s_bready,    // Indicate the master is ready to receive response
+
+//------------------------ Bram Interface -------------------------------------------//
+  output logic [DATA_WIDTH - 1 : 0] out_Di,   // Write data
+  input  logic [DATA_WIDTH - 1 : 0] in_Do,    // Read data
+  output logic [ADDR_WIDTH - 1 : 0] out_A,    // Address
+
+  output logic out_EN,   // Bram enable
+  
+  // Bram write enable (specific to which byte)
+  output logic [DATA_WIDTH / 8 - 1 : 0] out_WE
 );
 
   // +===========================================================================+
@@ -74,85 +84,113 @@ module axi4_lite_slave #(
   // +---------------------------------------------------------------------------|
   // |       01       |   EXOKAY | Exclusive access successful                   |
   // +---------------------------------------------------------------------------|
-  // |       10       |   SLVERR | Slave recieved access but returned error      |
+  // |       10       |   SLVERR | Slave received access but returned error      |
   // +---------------------------------------------------------------------------|
-  // |       11       |   DECERR | No slave at the transcation address           |
+  // |       11       |   DECERR | No slave at the transaction address           |
   // +===========================================================================+
 
-    localparam no_of_registers = 32;
+  // This module only generates 00 response because it is connected to a simple BRAM 
+  // and no error conditions are considered.
 
-    logic [DATA_WIDTH-1 : 0] register [no_of_registers-1 : 0];
-    logic [ADDR_WIDTH-1 : 0]    addr;
-    logic  write_addr;
-    logic  write_data;
+//------------------------ Handshake Signal -----------------------------------------//
+  logic raddr_hsked;
+  logic waddr_hsked;
+  logic rdata_hsked;
+  logic wdata_hsked;
+  logic wresp_hsked;
 
-    typedef enum logic [2 : 0] {IDLE,WRITE_CHANNEL,WRESP__CHANNEL, RADDR_CHANNEL, RDATA__CHANNEL} state_type;
-    state_type state , next_state;
+  assign raddr_hsked = in_s_arvalid && out_s_arready;
+  assign waddr_hsked = in_s_awvalid && out_s_awready;
+  assign rdata_hsked = out_s_rvalid && in_s_rready;
+  assign wdata_hsked = in_s_wvalid  && out_s_wready;
+  assign wresp_hsked = out_s_bvalid && in_s_bready;
+  
+//------------------------ State Machine --------------------------------------------//
+  localparam STATE_IDLE  = 2'd0;
+  localparam STATE_RDATA = 2'd1;
+  localparam STATE_WDATA = 2'd2;
+  
+  // We don't need another RRESP state because the response signal for read is
+  // generated at the same time with valid signal according to protocol.
+  localparam STATE_WRESP = 2'd3;
 
-    // AR
-    assign S_ARREADY = (state == RADDR_CHANNEL) ? 1 : 0;
-    // 
-    assign S_RVALID = (state == RDATA__CHANNEL) ? 1 : 0;
-    assign S_RDATA  = (state == RDATA__CHANNEL) ? register[addr] : 0;
-    assign S_RRESP  = (state == RDATA__CHANNEL) ?2'b00:0;
-    // AW
-    assign S_AWREADY = (state == WRITE_CHANNEL) ? 1 : 0;
-    // W
-    assign S_WREADY = (state == WRITE_CHANNEL) ? 1 : 0;
-    assign write_addr = S_AWVALID && S_AWREADY;
-    assign write_data = S_WREADY &&S_WVALID;
-    // B
-    assign S_BVALID = (state == WRESP__CHANNEL) ? 1 : 0;
-    assign S_BRESP  = (state == WRESP__CHANNEL )? 0:0;
+  logic [1 : 0] state_now;
+  logic [1 : 0] state_nxt;
 
-    integer i;
+  logic state_is_idle;
+  logic state_is_rdata_wait;
+  logic state_is_rdata;
+  logic state_is_wdata;
+  logic state_is_wresp;
 
-    always_ff @(posedge ACLK) begin
-        // Reset the register array
-        if (~ARESETN) begin
-            for (i = 0; i < 32; i++) begin
-                register[i] <= 32'b0;
-            end
-        end
-        else begin
-            if (state == WRITE_CHANNEL) begin
-                register[S_AWADDR] <= S_WDATA;
-            end
-            else if (state == RADDR_CHANNEL) begin
-                addr <= S_ARADDR;
-            end
-        end
-    end
+  assign state_is_idle       = (state_now == STATE_IDLE);
+  assign state_is_rdata      = (state_now == STATE_RDATA);
+  assign state_is_wdata      = (state_now == STATE_WDATA);
+  assign state_is_wresp      = (state_now == STATE_WRESP);
 
-    always_ff @(posedge ACLK) begin
-        if (!ARESETN) begin
-            state <= IDLE;
-        end
-        else begin
-            state <= next_state;
-        end
-    end
+  logic state_exit_ena;
+  logic state_idle_exit2rdata_ena;
+  logic state_idle_exit2wdata_ena;
+  logic state_rdwait_exit_ena;
+  logic state_rdata_exit_ena;
+  logic state_wdata_exit_ena;
+  logic state_wresp_exit_ena;
 
-    always_comb begin
-		case (state)
-            IDLE : begin
-                if (S_AWVALID) begin
-                    next_state = WRITE_CHANNEL;
-                end 
-                else if (S_ARVALID) begin
-                    next_state = RADDR_CHANNEL;
-                end 
-                else begin
-                    next_state = IDLE;
-                end
-            end
-            RADDR_CHANNEL   : if (S_ARVALID && S_ARREADY ) next_state = RDATA__CHANNEL;
-            RDATA__CHANNEL  : if (S_RVALID  && S_RREADY  ) next_state = IDLE;
-            WRITE_CHANNEL   : if (write_addr &&write_data) next_state = WRESP__CHANNEL;
-            WRESP__CHANNEL  : if (S_BVALID  && S_BREADY  ) next_state = IDLE;
-            default : next_state = IDLE;
-        endcase
-    end
+  assign state_exit_ena = state_idle_exit2rdata_ena 
+                       || state_idle_exit2wdata_ena
+                       || state_rdata_exit_ena 
+                       || state_wdata_exit_ena
+                       || state_wresp_exit_ena;
+  
+  assign state_idle_exit2rdata_ena = state_is_idle  && raddr_hsked;
+  assign state_idle_exit2wdata_ena = state_is_idle  && waddr_hsked;
+  assign state_rdata_exit_ena      = state_is_rdata && rdata_hsked;
+  assign state_wdata_exit_ena      = state_is_wdata && wdata_hsked;
+  assign state_wresp_exit_ena      = state_is_wresp && wresp_hsked;
+
+  assign state_nxt = ({2{state_idle_exit2rdata_ena}}  && STATE_RDATA     )
+                  || ({2{state_idle_exit2wdata_ena}}  && STATE_WDATA     )
+                  || ({2{state_rdata_exit_ena}}       && STATE_IDLE      )
+                  || ({2{state_wdata_exit_ena}}       && STATE_WRESP     )
+                  || ({2{state_wresp_exit_ena}}       && STATE_IDLE      );
+
+  always_ff @( posedge aclk or negedge aresetn ) begin : STATE_MACHINE
+    if (!aresetn) state_now <= 2'b0;
+    else if (state_exit_ena) state_now <= state_nxt;
+    else state_now <= state_now;
+  end
+
+//------------------------ Bram Interface -------------------------------------------//
+  assign out_A  = (in_s_araddr && {ADDR_WIDTH{state_idle_exit2rdata_ena}})
+               || (in_s_awaddr && {ADDR_WIDTH{state_idle_exit2wdata_ena}});
+
+  // EN need to last one more cycle for read because BRAM uses EN to assign Do
+  assign out_EN = state_idle_exit2rdata_ena || state_is_rdata;
+               || state_idle_exit2wdata_ena;
+
+  // Here we assume all the bytes will be valid when writing
+  // When state_is_wdata, all bytes will be written in to BRAM.
+  assign out_WE = {(DATA_WIDTH / 8){state_idle_exit2wdata_ena}};
+
+  assign out_Di = {DATA_WIDTH{state_idle_exit2wdata_ena}} & in_s_wdata;
+
+//------------------------ Master Interface -----------------------------------------//
+  assign out_s_rdata = {DATA_WIDTH{state_is_rdata}} & in_Do;
+  // We don't consider exclusive or error here because it's the simple BRAM
+  assign out_s_rresp = 2'b00;
+  assign out_s_bresp = 2'b00;
+
+  // Handshake signal
+  // Because this slave interface is for a simple BRAM, so the timing is predicted.
+  // They can be generated by the state machine, needn't to get any feedback from BRAM.
+  // However, for other memory device, these part of signals should be designed
+  // carefully.
+  assign out_s_arready = state_is_idle;
+  assign out_s_awready = state_is_idle;
+  assign out_s_rvalid  = state_is_rdata;
+  assign out_s_wready  = state_is_wdata;
+  assign out_s_bvalid  = state_is_wresp;
+
 endmodule
 
 
--- w-2_homework/time_borrow.sv
+++
@@ -1,8 +0,0 @@
-/*
-In order to satisfy all the flip-flops timing requirement, 
-which both paths should have a delay under 10, we need to 
-adjust some of the combinational logic part in path#2 and 
-put them in path#1. The ideal situation is both of them 
-have a delay of 9.
-
-*/
