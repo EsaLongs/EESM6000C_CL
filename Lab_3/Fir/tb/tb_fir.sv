929023a63016f3dc429835c7c0c179099492e1ec
EsaLongs <478113618@qq.com>
2025-03-11 17:16:28 +0800

    add some comment

--- Lab_3/Fir/tb/tb_fir.sv
+++ Lab_3/Fir/tb/tb_fir.sv
@@ -129,7 +129,7 @@ module fir_tb #(
     .WE   ( tap_WE   )
   );
 
-//------------------------ Prepare --------------------------------------------------//
+//------------------------ Data Prepare ---------------------------------------------//
   // **** Here "Din_list" will store the data waiting to be calculated. "golden_list"
   //      will store the expected calculation result.
   reg signed [ pDATA_WIDTH - 1 : 0] Din_list    [0 : DATA_NUM - 1];
@@ -140,7 +140,7 @@ module fir_tb #(
     $dumpvars();
   end
 
-  // **** Create clock
+  // **** Create clock.
   initial begin
     axis_clk = 0;
     forever begin
@@ -148,7 +148,7 @@ module fir_tb #(
     end
   end
 
-  // **** Reset
+  // **** Reset.
   initial begin
     axis_rst_n = 0;
     @(posedge axis_clk); 
@@ -156,6 +156,7 @@ module fir_tb #(
     axis_rst_n = 1;
   end 
 
+  // **** Read file.
   reg [31:0]  data_length;
   integer Din, golden, input_data, golden_data, m;
   initial begin
@@ -169,155 +170,171 @@ module fir_tb #(
     end
   end
 
-    integer i;
-    initial begin
-        $display("------------Start simulation-----------");
-        ss_tvalid = 0;
-        $display("----Start the data input(AXI-Stream)----");
-        for(i=0;i<(data_length-1);i=i+1) begin
-            ss_tlast = 0; ss(Din_list[i]);
-        end
-        config_read_check(32'h00000000, 32'h00, 32'h0000_000f); // check idle = 0
-        ss_tlast = 1; ss(Din_list[DATA_NUM - 1]);
-        $display("------End the data input(AXI-Stream)------");
+  // **** Tap RAM data.
+  reg signed [31:0] coef[0:10];
+  initial begin
+    coef[0]  =  32'd0;
+    coef[1]  = -32'd10;
+    coef[2]  = -32'd9;
+    coef[3]  =  32'd23;
+    coef[4]  =  32'd56;
+    coef[5]  =  32'd63;
+    coef[6]  =  32'd56;
+    coef[7]  =  32'd23;
+    coef[8]  = -32'd9;
+    coef[9]  = -32'd10;
+    coef[10] =  32'd0;
+  end
+
+  // **** Data RAM data.
+  integer i;
+  initial begin
+    $display("------------Start simulation-----------");
+    ss_tvalid = 0;
+    $display("----Start the data input(AXI-Stream)----");
+    for(i=0;i<(data_length-1);i=i+1) begin
+      // **** Write input data to data RAM.
+      ss_tlast = 0; ss(Din_list[i]);
     end
+    // **** Check configure register (check idle = 0).
+    config_read_check(32'h00000000, 32'h00, 32'h0000_000f);
+    ss_tlast = 1; ss(Din_list[DATA_NUM - 1]);
+    $display("------End the data input(AXI-Stream)------");
+  end
 
-    integer k;
-    reg error;
-    reg error_coef;
-    reg status_error;
-    initial begin
-        error = 0; status_error = 0;
-        sm_tready = 1;
-        wait (sm_tvalid);
-        for(k=0;k < data_length;k=k+1) begin
-            sm(golden_list[k],k);
-        end
-        config_read_check(32'h00000000, 32'h02, 32'h0000_0002); // check ap_done = 1 (0x00 [bit 1])
-        config_read_check(32'h00000000, 32'h04, 32'h0000_0004); // check ap_idle = 1 (0x00 [bit 2])
-        if (error == 0 & error_coef == 0) begin
-            $display("---------------------------------------------");
-            $display("-----------Congratulations! Pass-------------");
-        end
-        else begin
-            $display("--------Simulation Failed---------");
-        end
-        $finish;
+//------------------------ Simulate -------------------------------------------------//
+  integer k;
+  reg error;
+  reg error_coef;
+  reg status_error;
+
+  // **** Check Core Function.
+  initial begin
+    error = 0; 
+    status_error = 0;
+    sm_tready = 1;
+    wait (sm_tvalid);
+    for(k=0;k < data_length;k=k+1) begin
+      // **** Compare calculation result with golden result.
+      sm(golden_list[k],k);
     end
-
-    // Prevent hang
-    integer timeout = (1000000);
-    initial begin
-        while(timeout > 0) begin
-            @(posedge axis_clk);
-            timeout = timeout - 1;
-        end
-        $display($time, "Simualtion Hang ....");
-        $finish;
+    // **** Check configure register (check ap_done = 1) (check ap_idle = 1).
+    config_read_check(32'h00000000, 32'h02, 32'h0000_0002);
+    config_read_check(32'h00000000, 32'h04, 32'h0000_0004);
+    if (error == 0 & error_coef == 0) begin
+      $display("---------------------------------------------");
+      $display("-----------Congratulations! Pass-------------");
     end
-
-    reg signed [31:0] coef[0:10]; // fill in coef 
-    initial begin
-        coef[0]  =  32'd0;
-        coef[1]  = -32'd10;
-        coef[2]  = -32'd9;
-        coef[3]  =  32'd23;
-        coef[4]  =  32'd56;
-        coef[5]  =  32'd63;
-        coef[6]  =  32'd56;
-        coef[7]  =  32'd23;
-        coef[8]  = -32'd9;
-        coef[9]  = -32'd10;
-        coef[10] =  32'd0;
+    else begin
+      $display("--------Simulation Failed---------");
     end
+    $finish;
+  end
 
-
-    initial begin
-        error_coef = 0;
-        $display("----Start the coefficient input(AXI-lite)----");
-        config_write(32'h10000000, TAP_NUM);
-        config_write(32'h20000000, data_length);
-        for(k=0; k< TAP_NUM; k=k+1) begin
-            config_write(32'h30000000+k, coef[k]);
-        end
-        awvalid <= 0; wvalid <= 0;
-        // read-back and check
-        $display(" Check Coefficient ...");
-        for(k=0; k < TAP_NUM; k=k+1) begin
-            config_read_check(32'h30000000+k, coef[k], 32'hffffffff);
-        end
-        arvalid <= 0;
-        $display(" Tape programming done ...");
-        $display(" Start FIR");
-        @(posedge axis_clk) config_write(32'h00000000, 32'h0000_0001);    // ap_start = 1
-        $display("----End the coefficient input(AXI-lite)----");
+  // **** Check tap RAM written function and start simulation.
+  initial begin
+    error_coef = 0;
+    $display("----Start the coefficient input(AXI-lite)----");
+    // **** Write "TAP_NUM" and "DATA_NUM" to configure register.
+    config_write(32'h10000000, TAP_NUM);
+    config_write(32'h20000000, data_length);
+    for(k=0; k< TAP_NUM; k=k+1) begin
+    // **** Write tap data to tap RAM.
+      config_write(32'h30000000+k, coef[k]);
     end
+    awvalid <= 0; wvalid <= 0;
+    // **** Check data in tao RAM.
+    $display(" Check Coefficient ...");
+    for(k=0; k < TAP_NUM; k=k+1) begin
+      config_read_check(32'h30000000+k, coef[k], 32'hffffffff);
+    end
+    arvalid <= 0;
+    $display(" Tape programming done ...");
+    // **** Start simulation (set ap_start to be 1)
+    $display(" Start FIR");
+    @(posedge axis_clk) config_write(32'h00000000, 32'h0000_0001);
+    $display("----End the coefficient input(AXI-lite)----");
+  end
 
-    task config_write;
-        input [31:0]    addr;
-        input [31:0]    data;
-        begin
-            awvalid <= 0; wvalid <= 0;
-            arvalid <= 0; rready <= 0;
-            @(posedge axis_clk);
-            awvalid <= 1; awaddr <= addr;
-            wvalid  <= 1; wdata <= data;
-            @(posedge axis_clk);
-            while (!wready) @(posedge axis_clk);
-        end
-    endtask
+//------------------------ Prevent Hang ---------------------------------------------//
+  integer timeout = (1000000);
+  initial begin
+    while(timeout > 0) begin
+      @(posedge axis_clk);
+      timeout = timeout - 1;
+    end
+    $display($time, "Simualtion Hang ....");
+    $finish;
+  end
 
-    task config_read_check;
-        input [31:0]        addr;
-        input signed [31:0] exp_data;
-        input [31:0]        mask;
-        begin
-            arvalid <= 0;
-            awvalid <= 0; wvalid <= 0;
-            @(posedge axis_clk);
-            arvalid <= 1; araddr <= addr;
-            rready <= 1;
-            @(posedge axis_clk);
-            while (!rvalid) @(posedge axis_clk);
-            if( (rdata & mask) != (exp_data & mask)) begin
-                $display("ERROR: exp = %d, rdata = %d", exp_data, rdata);
-                error_coef <= 1;
-            end else begin
-                $display("OK: exp = %d, rdata = %d", exp_data, rdata);
-            end
-        end
-    endtask
+//------------------------ Task Define ----------------------------------------------//
+  //------------------------ Axi4 Lite Write ----------------------------------------//
+  task config_write;
+    input [31:0] addr;
+    input [31:0] data;
+    begin
+      awvalid <= 0; wvalid <= 0;
+      arvalid <= 0; rready <= 0;
+      @(posedge axis_clk);
+      awvalid <= 1; awaddr <= addr;
+      wvalid  <= 1; wdata <= data;
+      @(posedge axis_clk);
+      while (!wready) @(posedge axis_clk);
+    end
+  endtask
 
+  //------------------------ Axi4 Lite Read -----------------------------------------//
+  task config_read_check;
+    input        [31:0] addr;
+    input signed [31:0] exp_data;
+    input        [31:0] mask;
+    begin
+      arvalid <= 0;
+      awvalid <= 0; wvalid <= 0;
+      @(posedge axis_clk);
+      arvalid <= 1; araddr <= addr;
+      rready <= 1;
+      @(posedge axis_clk);
+      while (!rvalid) @(posedge axis_clk);
+      if( (rdata & mask) != (exp_data & mask)) begin
+        $display("ERROR: exp = %d, rdata = %d", exp_data, rdata);
+        error_coef <= 1;
+      end else begin
+        $display("OK: exp = %d, rdata = %d", exp_data, rdata);
+      end
+    end
+  endtask
 
+  //------------------------ Axi4 Stream Write (slave) ------------------------------//
+  task ss;
+    input  signed [31:0] in1;
+    begin
+      ss_tvalid <= 1;
+      ss_tdata  <= in1;
+      @(posedge axis_clk);
+      while (!ss_tready) begin
+        @(posedge axis_clk);
+      end
+    end
+  endtask
 
-    task ss;
-        input  signed [31:0] in1;
-        begin
-            ss_tvalid <= 1;
-            ss_tdata  <= in1;
-            @(posedge axis_clk);
-            while (!ss_tready) begin
-                @(posedge axis_clk);
-            end
-        end
-    endtask
-
-    task sm;
-        input  signed [31:0] in2; // golden data
-        input         [31:0] pcnt; // pattern count
-        begin
-            sm_tready <= 1;
-            @(posedge axis_clk) 
-            wait(sm_tvalid);
-            while(!sm_tvalid) @(posedge axis_clk);
-            if (sm_tdata != in2) begin
-                $display("[ERROR] [Pattern %d] Golden answer: %d, Your answer: %d", pcnt, in2, sm_tdata);
-                error <= 1;
-            end
-            else begin
-                $display("[PASS] [Pattern %d] Golden answer: %d, Your answer: %d", pcnt, in2, sm_tdata);
-            end
-            @(posedge axis_clk);
-        end
-    endtask
+  //------------------------ Axi4 Stream Write (master) -----------------------------//
+  task sm;
+    input signed [31:0] in2;  // golden data
+    input        [31:0] pcnt; // pattern count
+    begin
+      sm_tready <= 1;
+      @(posedge axis_clk) 
+      wait(sm_tvalid);
+      while(!sm_tvalid) @(posedge axis_clk);
+      if (sm_tdata != in2) begin
+        $display("[ERROR] [Pattern %d] Golden answer: %d, Your answer: %d", pcnt, in2, sm_tdata);
+        error <= 1;
+      end
+      else begin
+        $display("[PASS] [Pattern %d] Golden answer: %d, Your answer: %d", pcnt, in2, sm_tdata);
+      end
+      @(posedge axis_clk);
+    end
+  endtask
 endmodule
