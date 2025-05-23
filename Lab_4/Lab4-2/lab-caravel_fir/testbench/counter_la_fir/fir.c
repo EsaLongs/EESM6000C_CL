590f943ce011790ec078451be61ed775523e3d3c
EsaLongs <EsaLongs@noreply.gitcode.com>
2025-05-23 14:00:30 +0800

    update include.list

--- Lab_4/Lab4-2/lab-caravel_fir/testbench/counter_la_fir/fir.c
+++ Lab_4/Lab4-2/lab-caravel_fir/testbench/counter_la_fir/fir.c
@@ -16,17 +16,17 @@ void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
 int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
     
 	initfir();
-	//write down your fir
+	// ** write down your fir
 	reg_fir_control = 0x00000001;
 	
-	reg_mprj_datal = 0x00A50000; // start Testbench
+	reg_mprj_datal = 0x00A50000;  // start Testbench
 	
 	for (int i = 1; i <= N; i++) {
 		reg_fir_x = i;
 		outputsignal[i] = reg_fir_y;
 	}
 	
-	reg_mprj_datal = (outputsignal[N] << 24) + 0x005A0000; // end Testbench
+	reg_mprj_datal = (outputsignal[N] << 24) + 0x005A0000;  // end Testbench
 	
 	return outputsignal;
 }
